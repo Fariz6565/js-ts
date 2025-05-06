6a2cb251ee132fcf6a122d320f86aa36726a41db
Your Name <you@example.com>
2025-05-06 18:27:58 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// let x: number = 6;
+// console. log ( x )
+// let x: number = 4;
+// let y: number = 7;
+// let cavab: number = x+y;
+// console.log(cavab);
+// alert("Hello world")
+var input1 = +prompt("reqemi daxil et");
+var input2 = +prompt("reqemi daxil et");
+var cem = input1 * input2;
+console.log(cem);
+// let input: number=+prompt ("reqemi daxil et")
+// let input2: number=+prompt ("reqemi daxil et")
+// let cem:number=input+input2;
+// console.log(cem)
---
+++ index.ts
@@ -0,0 +1,24 @@
+// let x: number = 6;
+// console. log ( x )
+
+
+
+// let x: number = 4;
+// let y: number = 7;
+// let cavab: number = x+y;
+// console.log(cavab);
+
+
+// alert("Hello world")
+
+
+// let input1: number=+prompt ("reqemi daxil et")
+// let input2: number=+prompt ("reqemi daxil et")
+// let cem: number=input1*input2;
+// console.log(cem)
+
+
+// let input: number=+prompt ("reqemi daxil et")
+// let input2: number=+prompt ("reqemi daxil et")
+// let cem:number=input+input2;
+// console.log(cem)
